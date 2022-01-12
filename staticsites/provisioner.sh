a85e89ea8b35b565ddc221773741cffa67130bdf
Rudi MK <rudimk.cloud@gmail.com>
2022-01-12 18:26:32 +0530

    Removed support for injecting SSH keys due to some rather obvious and glaring security concerns.

--- staticsites/provisioner.sh
+++ staticsites/provisioner.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# SSH key housekeeping
-echo "Performing SSH key housekeeping..."
-sudo chmod 600 /home/vagrant/id_rsa
-eval `ssh-agent`
-ssh-add /home/vagrant/id_rsa
-
 # Install Nodejs 14.x
 echo "Installing Nodejs 14.x LTS..."
 source /home/vagrant/.nvm/nvm.sh && nvm install lts/fermium
