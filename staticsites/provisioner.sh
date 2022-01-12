f8d1113c8973f83bc3883f468a0ab115fd25304f
Rudi MK <rudimk.cloud@gmail.com>
2022-01-12 16:52:06 +0530

    Experimenting with adding a provisioner script to the staticsites image, which will be responsible for installing the latest Fermium LTS version of nodejs, as well as yarn.

---
+++ staticsites/provisioner.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Initialise nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install Nodejs 14.x
+
+nvm install lts/fermium
+
+# Install yarn
+
+npm install --global yarn
