8cce313671f5e97cb5220a99201d57806d9521eb
Rudi MK <rudimk.cloud@gmail.com>
2022-01-12 17:54:32 +0530

    Added support for copying SSH keys mounted as Docker secrets, to the SSH dir for the vagrant user, as well as making them usable within the provisioner.

--- staticsites/provisioner.sh
+++ staticsites/provisioner.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# Initialise nvm
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# SSH key housekeeping
+sudo chmod 600 /home/vagrant/id_rsa
+eval `ssh-agent`
+ssh-add /home/vagrant/id_rsa
 
 # Install Nodejs 14.x
 
-nvm install lts/fermium
+source /home/vagrant/.nvm/nvm.sh && nvm install lts/fermium
 
 # Install yarn
 
