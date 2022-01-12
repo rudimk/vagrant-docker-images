a3adc1fb228d648634e9dcc80206c2f1a115dc47
Rudi MK <rudimk.cloud@gmail.com>
2022-01-12 18:03:01 +0530

    Added logs.

--- staticsites/provisioner.sh
+++ staticsites/provisioner.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # SSH key housekeeping
+echo "Performing SSH key housekeeping..."
 sudo chmod 600 /home/vagrant/id_rsa
 eval `ssh-agent`
 ssh-add /home/vagrant/id_rsa
 
 # Install Nodejs 14.x
-
+echo "Installing Nodejs 14.x LTS..."
 source /home/vagrant/.nvm/nvm.sh && nvm install lts/fermium
 
 # Install yarn
-
+echo "Installing yarn..."
 npm install --global yarn
