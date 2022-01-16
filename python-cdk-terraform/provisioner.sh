de3e2afc4b32062448825a1f1362e905a3b35472
Rudi MK <rudimk.cloud@gmail.com>
2022-01-16 11:18:39 +0530

    Fixed bugs with the provisioner for the vagrant-docker image where SSH keys were not being added to the SSH agent correctly.

--- python-cdk-terraform/provisioner.sh
+++ python-cdk-terraform/provisioner.sh
@@ -2,13 +2,12 @@
 
 # Generate SSH key
 echo "Generating new SSH ED25519 key ==>"
-ssh-keygen -t ed25519 -C "rudimk.cloud@gmail.com" -f ~/. ssh/id_ed25519 -q -N ""
+su - vagrant -c 'ssh-keygen -t ed25519 -C "rudimk.cloud@gmail.com" -f /home/vagrant/.ssh/id_ed25519 -q -N ""'
 
 # Add SSH key to the SSH agent
 echo "Adding the key to the SSH agent ==>"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+su - vagrant -c 'eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519'
 
 # Print public key
 echo "Public key ==>"
-cat ~/.ssh/id_ed25519.pub
+su - vagrant -c 'cat ~/.ssh/id_ed25519.pub'
