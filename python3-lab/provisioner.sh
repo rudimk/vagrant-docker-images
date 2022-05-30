c7ed6217a1a87bacbb1eafd26043b700bf82f598
Rudi MK <rudimk.cloud@gmail.com>
2022-05-30 15:06:56 +0530

    Added a Dockerfile, a default Vagrantfile and a Vagrant provisioner for a Python 3.8 dev environment.

---
+++ python3-lab/provisioner.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Generate SSH key
+echo "Generating new SSH ED25519 key ==>"
+su - vagrant -c 'ssh-keygen -t ed25519 -C "rudimk.cloud@gmail.com" -f /home/vagrant/.ssh/id_ed25519 -q -N ""'
+
+# Add SSH key to the SSH agent
+echo "Adding the key to the SSH agent ==>"
+su - vagrant -c 'eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519'
+
+# Print public key
+echo "Public key ==>"
+su - vagrant -c 'cat ~/.ssh/id_ed25519.pub'
